e2061b0c20126e705233ee5b6f8f0ca28ac3b5ac
Andres <andresnrx@gmail.com>
2018-12-02 04:57:02 -0800

    split up and make a macro because why not

---
+++ src/common/lib.rs
@@ -0,0 +1,6 @@
+pub mod input_fetcher;
+pub mod sanitize;
+#[macro_use]
+pub mod macros;
+
+pub use self::macros::*;
---
+++ src/common/macros.rs
@@ -0,0 +1,11 @@
+#[macro_export]
+macro_rules! bootstrap {
+    ($x:expr) => {
+        fn main() {
+            use $crate::input_fetcher;
+            
+            let input = input_fetcher::fetch_or_exit($x);
+            println!("{}", solve(input.as_str()));
+        }
+    };
+}
