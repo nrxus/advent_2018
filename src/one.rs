4654c9de5c3a74c5938fe13018c4a9744fb71d80
Andres <andresnrx@gmail.com>
2018-12-02 02:02:54 -0800

    1.a

--- src/one.rs
+++ src/one.rs
@@ -2,8 +2,21 @@ mod input_fetcher;
 
 fn main() {
     let input = input_fetcher::fetch_or_exit(1);
-    println!("{:?}", input);
+    println!("{}", a(input.as_str()));
+}
+
+fn a(input: &str) -> i32 {
+    input.lines().map(|l| l.parse::<i32>().unwrap()).sum()
 }
 
 #[cfg(test)]
-mod test {}
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_a() {
+        assert_eq!(a("+1\n+1\n+1"), 3);
+        assert_eq!(a("+1\n+1\n-2"), 0);
+        assert_eq!(a("-1\n-2\n-3"), -6);
+    }
+}
