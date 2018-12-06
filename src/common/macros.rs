7f3a9babc09330adbbb6b297858909fa983e665e
Andres <andresnrx@gmail.com>
2018-12-05 17:58:51 -0800

    four b

--- src/common/macros.rs
+++ src/common/macros.rs
@@ -3,7 +3,7 @@ macro_rules! bootstrap {
     ($x:expr) => {
         fn main() {
             use $crate::input_fetcher;
-            
+
             let input = input_fetcher::fetch_or_exit($x);
             println!("{}", solve(input.as_str()));
         }
--- src/four_b.rs
+++ src/four_b.rs
@@ -4,40 +4,33 @@
 mod guard_schedule;
 
 use self::guard_schedule::*;
-
 use std::{collections::HashMap, convert::TryFrom};
 
-fn solve(input: &str) -> u16 {
+fn solve(input: &str) -> u32 {
     let mut entries: Vec<_> = input.lines().map(|l| Entry::try_from(l).unwrap()).collect();
     entries.sort_unstable_by(|a, b| a.date().cmp(b.date()));
-    let guards = Schedule::guard_schedules(&entries).unwrap();
 
-    let (id, schedules) = guards
+    Schedule::guard_schedules(&entries)
+        .unwrap()
         .iter()
-        .max_by_key(|(_, schedules)| {
-            schedules
+        .filter_map(|(id, schedules)| {
+            let asleep_times = schedules
                 .iter()
-                .flat_map(|s| s.minutes.iter())
-                .filter(|&&s| s == GuardState::Asleep)
-                .count()
-        })
-        .unwrap();
+                .flat_map(Schedule::asleep_minutes)
+                .fold(HashMap::<usize, u16>::new(), |mut acc, m| {
+                    let count = acc.entry(m).or_default();
+                    *count += 1;
+                    acc
+                })
+                .iter()
+                .max_by_key(|(_, &c)| c)
+                .map(|(&a, &b)| (a, b));
 
-    let minute = schedules
-        .iter()
-        .flat_map(Schedule::asleep_minutes)
-        .fold(HashMap::<usize, u16>::new(), |mut acc, m| {
-            let count = acc.entry(m).or_default();
-            *count += 1;
-            acc
+            asleep_times.map(|t| (id, t))
         })
-        .iter()
-        .max_by_key(|(_, &c)| c)
-        .map(|(m, _)| m)
-        .cloned()
-        .unwrap();
-
-    id * minute as u16
+        .max_by_key(|(_, (_, count))| *count)
+        .map(|(&id, (minute, _))| u32::from(id) * minute as u32)
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -47,24 +40,24 @@ mod test {
     #[test]
     fn test() {
         let input = r"[1518-11-01 00:00] Guard #10 begins shift
+[1518-11-01 00:05] falls asleep
 [1518-11-01 00:25] wakes up
 [1518-11-01 00:30] falls asleep
 [1518-11-01 00:55] wakes up
 [1518-11-01 23:58] Guard #99 begins shift
-[1518-11-01 00:05] falls asleep
 [1518-11-02 00:40] falls asleep
 [1518-11-02 00:50] wakes up
+[1518-11-03 00:05] Guard #10 begins shift
 [1518-11-03 00:24] falls asleep
 [1518-11-03 00:29] wakes up
 [1518-11-04 00:02] Guard #99 begins shift
-[1518-11-03 00:05] Guard #10 begins shift
 [1518-11-04 00:36] falls asleep
 [1518-11-04 00:46] wakes up
 [1518-11-05 00:03] Guard #99 begins shift
 [1518-11-05 00:45] falls asleep
 [1518-11-05 00:55] wakes up";
 
-        assert_eq!(solve(input), 240);
+        assert_eq!(solve(input), 4455);
     }
 }
 
